1b76b4aa37d8bfacd35f41ba47e5e13cb4da9bb0
Abdullah Al Noman <imnomanmohammad19@gmail.com>
2024-07-07 13:14:13 +0600

    Added Single Line and Multiple Line Comment

--- comments.c
+++ comments.c
@@ -4,3 +4,7 @@ int main()
     printf("Comments and Escape Sequences");
     return 0;
 }
+//* This is a oneline comment. In one line comment, we have to use two forward slash to make this command.
+/* This is a multiple line comment. There are two types of comment here. One is Single line
+comment and another is Multiple line comment. In Multiple Line Comment, We have to use One forward slash, then one star
+and again one star and a forward slash at the end. */
