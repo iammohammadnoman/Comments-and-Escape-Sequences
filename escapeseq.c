96d52cfecb46bf9e98b6989d24d2152eee20d165
Abdullah Al Noman <imnomanmohammad19@gmail.com>
2024-07-07 17:45:43 +0600

    First Commit for Escape Sequences

---
+++ escapeseq.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    printf("Escape Sequences \n\n");
+    printf("Escape sequences are special character combinations that are used to represent characters, \n that are not directly typable or printable. \n They begin with a backslash followed by a character.");   
+    return 0;
+}
+//TODO Remember: Here are some common escape sequences in C -
+//? Newline Character = \n 
+//? Horizontal Tab Character = \t
+//? Vertical Tab Character = \v
+//? Double Quotation Mark = \"
+//? Single Quotation Mark = \'
+//? Backslash = \\ 
+//? (Alert)Bell = \a
+//? Backspace = \b
+//? Form Feed = \f
+//? Carriage Return = \r
+//? Null Character = \0
+//? Question Mark = \?
+
+//** They're generally worked as output function. */
