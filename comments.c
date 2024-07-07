bb4c0b3f23df8c7a9cbc6b4ef77b7d2edd5b4f43
Abdullah Al Noman <imnomanmohammad19@gmail.com>
2024-07-07 15:23:47 +0600

    Added Todo Remember and Alerts

--- comments.c
+++ comments.c
@@ -8,3 +8,7 @@ int main()
 /* This is a multiple line comment. There are two types of comment here. One is Single line
 comment and another is Multiple line comment. In Multiple Line Comment, We have to use One forward slash, then one star
 and again one star and a forward slash at the end. */
+
+//*TODO Remember
+//? Single line comment was created by C++. It's use in C programming is technically invalid, but some compilers will accept it.
+//! In C, you can't have one comment within another comment.Because, It will be a syntax error.
